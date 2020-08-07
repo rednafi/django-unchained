226214eab28b370240b2a42470ebc3cbfd93fbe0
rednafi <redowan.nafi@gmail.com>
2020-08-08 04:18:56 +0600

    Added testcase for pages app

--- project_2_pages_app/pages/tests.py
+++ project_2_pages_app/pages/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+"""Since this project doesn't involve any database, so we're using
+SimpleTestCase instead of TestCase"""
+
+from django.test import SimpleTestCase
 
 # Create your tests here.
+
+class SimpleTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page_status_code(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code,200)
