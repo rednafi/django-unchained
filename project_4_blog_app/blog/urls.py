926c600a3315b98dee7b278eae2d6a791c493e89
rednafi <redowan.nafi@gmail.com>
2020-08-09 13:47:13 +0600

    Completed project 4 blog app

--- project_4_blog_app/blog/urls.py
+++ project_4_blog_app/blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path("", views.BlogListView.as_view(), name="home"),
+    path("post/<int:pk>/", views.BlogDetailView.as_view(), name="post_detail"),
 ]
--- project_4_blog_app/blog/views.py
+++ project_4_blog_app/blog/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 from .models import Post
+
 # Create your views here.
+
 
 class BlogListView(ListView):
     model = Post
-    template_name = 'home.html'
+    template_name = "home.html"
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    tempate_name = "post_detail.html"
