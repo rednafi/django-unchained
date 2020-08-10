02a76822e575629948bcbc66fdef1183c31b6a27
rednafi <redowan.nafi@gmail.com>
2020-08-10 13:25:45 +0600

    Added tests

--- project_4_blog_app/blog/tests.py
+++ project_4_blog_app/blog/tests.py
@@ -40,3 +40,23 @@ class BlogTests(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, "A good day to die")
         self.assertTemplateUsed(response, "blog/post_detail.html")
+
+    def test_post_create_view(self):
+        response = self.client.post(
+            reverse("post_new"),
+            {"title": "New title", "body": "New text", "author": self.user},
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "New title")
+        self.assertContains(response, "New text")
+
+    def test_post_update_view(self):
+        response = self.client.post(
+            reverse("post_edit", args="1"),
+            {"title": "Updated title", "body": "Updated text",},
+        )
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_delete_view(self):
+        response = self.client.get(reverse("post_delete", args="1"))
+        self.assertEqual(response.status_code, 200)
