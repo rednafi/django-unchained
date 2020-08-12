9c4ffd7a4b7cfc1f039e967785958b1434ec3abe
rednafi <redowan.nafi@gmail.com>
2020-08-13 02:24:00 +0600

    Added bootstrap templates

--- project_5_newspaper_app/project_5_newspaper_app/settings.py
+++ project_5_newspaper_app/project_5_newspaper_app/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # local
     "users",
     "pages",
+    # third party
+    "crispy_forms",
 ]
 
 MIDDLEWARE = [
@@ -117,3 +120,4 @@ STATIC_URL = "/static/"
 AUTH_USER_MODEL = "users.CustomUser"
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
