696a4bc240606f58ed8d37643d7df3dc2ae9c3f9
rednafi <redowan.nafi@gmail.com>
2020-08-17 23:07:25 +0600

    Add docker-compose

---
+++ django_for_professionals/project_1_hello_world/pages/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from pages.views import home_page_view
+
+urlpatterns = [
+    path("", home_page_view, name="home"),
+]
--- django_for_professionals/project_1_hello_world/pages/views.py
+++ django_for_professionals/project_1_hello_world/pages/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+def home_page_view(request):
+    return HttpResponse('Hello, World!')
