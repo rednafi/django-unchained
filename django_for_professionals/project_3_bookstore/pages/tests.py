6431fe112741496c3c9031d850e32a27b8632d41
rednafi <redowan.nafi@gmail.com>
2020-08-20 01:35:18 +0600

    Completed bootstrap uplifting

--- django_for_professionals/project_3_bookstore/pages/tests.py
+++ django_for_professionals/project_3_bookstore/pages/tests.py
@@ -1,4 +1,4 @@
-from .views import HomePageView
+from .views import HomePageView, AboutPageView
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 
@@ -9,9 +9,6 @@ class HomepageTests(SimpleTestCase):
         self.response = self.client.get(url)
 
     def test_homepage_status_code(self):
-        self.assertEqual(self.response.status_code, 200)
-
-    def test_homepage_url_name(self):
         self.assertEqual(self.response.status_code, 200)
 
     def test_homepage_template(self):
@@ -26,3 +23,25 @@ class HomepageTests(SimpleTestCase):
     def test_homepage_url_resolves_homepageview(self):
         view = resolve("/")
         self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
+
+
+class AboutPageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse("about")
+        self.response = self.client.get(url)
+
+    def test_aboutpage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_aboutpage_template(self):
+        self.assertTemplateUsed(self.response, "about.html")
+
+    def test_aboutpage_contains_correct_html(self):
+        self.assertContains(self.response, "About")
+
+    def test_aboutpage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, "Hi there! I shouldn't be on the page")
+
+    def test_aboutpage_url_resolves_homepageview(self):
+        view = resolve("/about/")
+        self.assertEqual(view.func.__name__, AboutPageView.as_view().__name__)
--- django_for_professionals/project_3_bookstore/project_3_bookstore/settings.py
+++ django_for_professionals/project_3_bookstore/project_3_bookstore/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # third party
+    "crispy_forms",
     # local
     "users.apps.UsersConfig",
     "pages.apps.PagesConfig",
@@ -119,6 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# Custom user model
 AUTH_USER_MODEL = "users.CustomUser"
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+
+# Django crispy forms
+CRISPY_TEMPLATE_PACK = "bootstrap4"
