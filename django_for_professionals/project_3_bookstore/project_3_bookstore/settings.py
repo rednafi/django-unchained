3252e857638857641e1f9dc912920de74c1f2784
rednafi <redowan.nafi@gmail.com>
2020-08-19 12:51:31 +0600

    Added pages app

--- django_for_professionals/project_3_bookstore/project_3_bookstore/settings.py
+++ django_for_professionals/project_3_bookstore/project_3_bookstore/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # local
     "users.apps.UsersConfig",
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
--- django_for_professionals/project_3_bookstore/users/tests.py
+++ django_for_professionals/project_3_bookstore/users/tests.py
@@ -9,9 +9,8 @@ class CustomUserTests(TestCase):
             username="testuser", email="testuser@testmail.com", password="testpass",
         )
 
-        self.assertEquals(user.username, "testuser")
-        self.assertEquals(user.email, "testuser@testmail.com")
-        self.assertEquals(user.password, "testpass")
+        self.assertEqual(user.username, "testuser")
+        self.assertEqual(user.email, "testuser@testmail.com")
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_staff)
         self.assertFalse(user.is_superuser)
@@ -23,9 +22,8 @@ class CustomUserTests(TestCase):
             username="superadmin", email="superadmin@email.com", password="superpass"
         )
 
-        self.assertEquals(admin_user.username, "superadmin")
-        self.assertEquals(admin_user.email, "superadmin@email.com")
-        self.assertEqual(admin_user.password, "superpass")
+        self.assertEqual(admin_user.username, "superadmin")
+        self.assertEqual(admin_user.email, "superadmin@email.com")
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
